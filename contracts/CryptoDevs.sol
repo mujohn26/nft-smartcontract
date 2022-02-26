797c2682c9851ed54154aeaf9a391ac145f120d1
MUGIRANEZA JOHN <52497006+mujohn26@users.noreply.github.com>
2022-02-26 23:47:36 +0200

    add smart contract[WIP]

---
+++ contracts/CryptoDevs.sol
@@ -0,0 +1,11 @@
+ // SPDX-License-Identifier: MIT
+  pragma solidity ^0.8.4;
+
+  import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+  import "@openzeppelin/contracts/access/Ownable.sol";
+  import "./IWhitelist.sol";
+
+  contract CryptoDevs is ERC721Enumerable, Ownable{
+    
+  }
+
