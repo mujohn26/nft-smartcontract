d01d219e9d17563e48d206adeebf2c5d2d65bc68
MUGIRANEZA JOHN <52497006+mujohn26@users.noreply.github.com>
2022-02-27 22:23:39 +0200

    nft contract

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x7494127086e42B302165909785dd51e054Fd6dDa";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
